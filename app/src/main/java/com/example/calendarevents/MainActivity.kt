0cffe14fd8b8927aadad9b27dbb6985a88a27a75
emmanuel <sunartio.emmanuel@gmail.com>
2023-01-02 17:55:16 +1030

    created MaterialCalendarView - basic

--- app/src/main/java/com/example/calendarevents/MainActivity.kt
+++ app/src/main/java/com/example/calendarevents/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calendarevents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calendarevents.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var calendarView: MaterialCalendarView
+    private lateinit var fabAddEvent: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fabAddEvent = binding.fabAddEvent
+        calendarView = binding.contentMain.calendarView
     }
 }
